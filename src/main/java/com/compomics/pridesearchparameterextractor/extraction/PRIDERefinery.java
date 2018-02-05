b1747755337befcc6526023f1599a64d7216a7ad
JavaSaurus <javasaurusdev@gmail.com>
2018-02-05 09:44:39 +0100

    Revert "refinement of Phosphorylation parameters"
    This reverts commit c69c6da85e0b6cb5e999509f18dba960bcf8c5a9.

--- src/main/java/com/compomics/pridesearchparameterextractor/extraction/PRIDERefinery.java
+++ src/main/java/com/compomics/pridesearchparameterextractor/extraction/PRIDERefinery.java
@@ -1,16 +1,12 @@
 package com.compomics.pridesearchparameterextractor.extraction;
 
 import com.compomics.pride_asa_pipeline.core.data.extractor.IParametersRefinery;
-import com.compomics.util.experiment.biology.PTMFactory;
-import com.compomics.util.experiment.identification.identification_parameters.PtmSettings;
 import com.compomics.util.experiment.identification.identification_parameters.SearchParameters;
 import com.compomics.util.experiment.identification.ptm.PtmScore;
 import com.compomics.util.preferences.IdMatchValidationPreferences;
 import com.compomics.util.preferences.IdentificationParameters;
 import com.compomics.util.preferences.PTMScoringPreferences;
 import org.apache.log4j.Logger;
-
-import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -30,29 +26,14 @@ public class PRIDERefinery implements IParametersRefinery{
 
     private static final Logger LOGGER = Logger.getLogger(PRIDERefinery.class);
 
-    private static final PTMFactory ptmFactory = PTMFactory.getInstance();
-
-    private String[] canonicalMods = new String[]{"Phosphorylation of T", "Phosphorylation of S", "Phosphorylation of Y"};
-
     @Override
     public IdentificationParameters refineIdentificationParameters(IdentificationParameters identificationParameters) {
 
-        /**
-         * Refining modifications, Some times the modifications, specially phosphorylation is not captured in the righ
-         * way.
-         */
-
-        SearchParameters searchParameters = identificationParameters.getSearchParameters();
-        PtmSettings searchPTMs = searchParameters.getPtmSettings();
-        searchPTMs = correctCanonicalPhosphorylation(searchPTMs);
-        searchParameters.setPtmSettings(searchPTMs);
-        identificationParameters.setSearchParameters(searchParameters);
+        PTMScoringPreferences ptmScoringParams = identificationParameters.getPtmScoringPreferences();
 
         /**
          * Define the probability scores for the Phosphorylation
          */
-        PTMScoringPreferences ptmScoringParams = identificationParameters.getPtmScoringPreferences();
-
         ptmScoringParams.setProbabilitsticScoreCalculation(true);
         ptmScoringParams.setSelectedProbabilisticScore(PtmScore.PhosphoRS);
         identificationParameters.setPtmScoringPreferences(ptmScoringParams);
@@ -62,44 +43,14 @@ public class PRIDERefinery implements IParametersRefinery{
          */
 
         IdMatchValidationPreferences qualityParams = identificationParameters.getIdValidationPreferences();
-        qualityParams.setDefaultPsmFDR(0.2);
-        qualityParams.setDefaultPeptideFDR(0.2);
-        qualityParams.setDefaultProteinFDR(0.1);
+        qualityParams.setDefaultPsmFDR(0.1);
+        qualityParams.setDefaultPeptideFDR(0.1);
+        qualityParams.setDefaultProteinFDR(0.01);
         identificationParameters.setIdValidationPreferences(qualityParams);
-
-
 
         LOGGER.info("Final Parameters after refinements -- " + identificationParameters.toString());
 
         return identificationParameters;
-    }
-
-    /**
-     * This fucntion corrects if the parameters has modifications in phosphorylation,
-     * it should look for all of them (e.g S, T , Y)
-     * @param searchPTMs
-     * @return
-     */
-    private PtmSettings correctCanonicalPhosphorylation(PtmSettings searchPTMs) {
-
-        boolean canonicalPhosphoPresent = false;
-
-        ArrayList<String> modifications = searchPTMs.getAllModifications();
-        for(String mod: modifications){
-            for(String canonical: canonicalMods){
-                if(canonical.equalsIgnoreCase(mod)){
-                    canonicalPhosphoPresent = true;
-                    break;
-                }
-            }
-        }
-        if(canonicalPhosphoPresent){
-            for(String mod: canonicalMods)
-                if(!searchPTMs.contains(mod))
-                    searchPTMs.addVariableModification(ptmFactory.getPTM(mod));
-        }
-
-        return searchPTMs;
     }
 
 
